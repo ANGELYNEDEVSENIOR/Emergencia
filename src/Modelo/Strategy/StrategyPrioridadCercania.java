6df4c2a0b2dbe54634b4425ed262d137b92ae579
Angelyne Mariana Gonzalez Pinto <angelynemarianagonzalespinto@gmail.com>
2025-01-26 16:13:00 -0500

    el singleton

--- src/Modelo/Strategy/StrategyPrioridadCercania.java
+++ src/Modelo/Strategy/StrategyPrioridadCercania.java
@@ -1,5 +1,32 @@
 package Modelo.Strategy;
 
+import Modelo.Emergencia;
+
 public class StrategyPrioridadCercania implements StrategyPrioridad {
 //creacion de clase interna mapa urbano
+private class MapaUrbano {
+        public int calcularDistancia(String ubicacion) {
+            switch (ubicacion.toLowerCase()) {
+                case "ZONA NORTE":
+                return 8;    
+                case "ZONA SUR":
+                return 10; 
+                case "ZONA CENTRO":
+                return 2; 
+                case "ZONA ORIENTE":
+                return 5;
+                case "ZONA OCCIDENTE":
+                return 6;  
+                default:
+                    return 10;
+  //estimacion de la distancia
+            }
+        }
+}
+public MapaUrbano mapaUrbano = new MapaUrbano();
+@Override
+public int calcularPrioridad(Emergencia emergencia) {
+  int calcularDistancia = mapaUrbano.calcularDistancia(emergencia.getUbicacion());
+  return 10 - calcularDistancia;
+}
 }
